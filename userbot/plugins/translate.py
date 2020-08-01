65738e1cf1598949f9c774001834bfc3a8a37a73
Jackyj09 <56272417+Jackyj09@users.noreply.github.com>
2020-08-01 22:52:14 +0530

    Update translate.py

--- userbot/plugins/translate.py
+++ userbot/plugins/translate.py
@@ -1,44 +1,58 @@
-""" Google Translate
-Available Commands:
-.tr LanguageCode as reply to a message
-.tr LangaugeCode | text to translate"""
+# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+#
+# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
+#
+# All rights reserved.
 
-import emoji
-from googletrans import Translator
-from userbot.utils import admin_cmd
+from json import dumps
+from emoji import get_emoji_regexp
+from googletrans import Translator, LANGUAGES
+from userge import userge, Message, Config
 
 
-@borg.on(admin_cmd("tr ?(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    if "trim" in event.raw_text:
-        # https://t.me/c/1220993104/192075
-        return
-    input_str = event.pattern_match.group(1)
-    if event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        text = previous_message.message
-        lan = input_str or "ml"
-    elif "|" in input_str:
-        lan, text = input_str.split("|")
-    else:
-        await event.edit("`.tr LanguageCode` as reply to a message")
-        return
-    text = emoji.demojize(text.strip())
-    lan = lan.strip()
+@userge.on_cmd("tr", about={
+    'header': "Translate the given text using Google Translate",
+    'supported languages': dumps(LANGUAGES, indent=4, sort_keys=True),
+    'usage': "from english to sinhala\n"
+             "{tr}tr -en -si i am userge\n\n"
+             "from auto detected language to sinhala\n"
+             "{tr}tr -si i am userge\n\n"
+             "from auto detected language to preferred\n"
+             "{tr}tr i am userge\n\n"
+             "reply to message you want to translate from english to sinhala\n"
+             "{tr}tr -en -si\n\n"
+             "reply to message you want to translate from auto detected language to sinhala\n"
+             "{tr}tr -si\n\n"
+             "reply to message you want to translate from auto detected language to preferred\n"
+             "{tr}tr"}, del_pre=True)
+async def translateme(message: Message):
     translator = Translator()
+    text = message.filtered_input_str
+    flags = message.flags
+    if message.reply_to_message:
+        text = message.reply_to_message.text
+    if not text:
+        await message.err(
+            text="Give a text or reply to a message to translate!\nuse `.help tr`")
+        return
+    if len(flags) == 2:
+        src, dest = list(flags)
+    elif len(flags) == 1:
+        src, dest = 'auto', list(flags)[0]
+    else:
+        src, dest = 'auto', Config.LANG
+    text = get_emoji_regexp().sub(u'', text)
+    await message.edit("Translating...")
     try:
-        translated = translator.translate(text, dest=lan)
-        after_tr_text = translated.text
-        # TODO: emojify the :
-        # either here, or before translation
-        output_str = """**TRANSLATED** from {} to {}
-{}""".format(
-            translated.src,
-            lan,
-            after_tr_text
-        )
-        await event.edit(output_str)
-    except Exception as exc:
-        await event.edit(str(exc))
+        reply_text = translator.translate(
+            text, dest=dest, src=src)
+    except ValueError:
+        await message.err(text="Invalid destination language.\nuse `.help tr`")
+        return
+    source_lan = LANGUAGES[f'{reply_text.src.lower()}']
+    transl_lan = LANGUAGES[f'{reply_text.dest.lower()}']
+    output = f"**Source ({source_lan.title()}):**`\n{text}`\n\n\
+**Translation ({transl_lan.title()}):**\n`{reply_text.text}`"
+    await message.edit_or_send_as_file(text=output, caption="translated")
